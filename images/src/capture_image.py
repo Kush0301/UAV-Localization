bdeb014e1e82784a55db7d74eb61e21a3b5510f2
Kushagra <kushagra2000@gmail.com>
2020-08-31 20:57:45 +0530

    Image subscriber to camera feed

---
+++ images/src/capture_image.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+
+bridge = CvBridge()
+
+def image_callback(ros_image):
+  print ('got an image')
+  global bridge
+  try:
+    cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+  except CvBridgeError as e:
+      print(e)
+
+  (rows,cols,channels) = cv_image.shape
+  cv2.imshow("Image window", cv_image)
+  cv2.waitKey(3)
+
+  
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  #for turtlebot3 waffle
+  #image_topic="/camera/rgb/image_raw/compressed"
+  #for usb cam
+  #image_topic="/usb_cam/image_raw"
+  image_sub = rospy.Subscriber("/webcam/image_raw",Image, image_callback)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
