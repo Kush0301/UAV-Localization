e8728ad2aad0f78564e37eb5c92badc153a73d01
Kushagra <kushagra2000@gmail.com>
2020-11-12 22:46:18 +0530

    Fixed few bugs

--- images/src/visual_odom.py
+++ images/src/visual_odom.py
@@ -2,10 +2,10 @@
 
 import rospy
 import cv2
+import os
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import CameraInfo, Image
-
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, euler_matrix, euler_from_matrix
 import numpy as np
 import os
@@ -13,21 +13,152 @@ import sys
 from features import *
 from cv_bridge import CvBridge
 
+bridge=CvBridge()
+motion=0
+threshold=1000
+pixdiff=5
+odom_pub=rospy.Publisher("visual_odom", Odometry, queue_size=10)
+k=0
+i_pose=Odometry()
+odom=Odometry()
+image_ref=0
+translation=0
+rotation=0
+new_cloud=0
+kp2=0
+image_next=0
 
-def initialize():
-    odom_pub=rospy.Publisher("visual_odom", Odometry, queue_size=10)
-    pixdiff=2
-    bridge=CvBridge()
+def publisher(time, translation, rotation):
     
-    #Camera info
-    camera_info=rospy.wait_for_message("/webcam/camera_info",CameraInfo)
+    print("Time:")
+    print(time)
+    print("Translation")
+    print(translation)
+    print("Rotation")
+    print(rotation)
+
+    global odom_pub,odom
+
+    rotation1=np.hstack((rotation,np.zeros((3,1))))
+    rotation1=np.vstack((rotation1,np.array([0,0,0,1]).reshape(1,4)))
+    euler=list(euler_from_matrix(rotation1,'sxyz'))
+    quat=quaternion_from_euler(euler[0],euler[1],euler[2])
+    position=list(translation.reshape(3,))
+    np.random.seed(1)
+    p_cov=np.random.random((6,6))
+    odom.header.stamp.secs=time
+    odom.child_frame_id = "base_link"
+    odom.header.frame_id = "visual_odom"
+    odom.pose.pose.position.x=position[0]
+    odom.pose.pose.position.y=position[1]
+    odom.pose.pose.position.z=position[2]
+    odom.pose.pose.orientation.x=quat[0]
+    odom.pose.pose.orientation.y=quat[1]
+    odom.pose.pose.orientation.z=quat[2]
+    odom.pose.pose.orientation.w=quat[3]
+    odom.pose.covariance=list(p_cov.reshape(36,))
+    odom_pub.publish(odom)
+
+def initialize(image_ref,ros_image):
+
+    global bridge
+    kp1=extract_features(image_ref)
+    
+    image_next=bridge.imgmsg_to_cv2(ros_image, "bgr8")
+    kp1,kp2,diff=track_features(image_ref, image_next, kp1)
+    return kp1,kp2,diff
+
+
+def image_callback(ros_image):
+    
+    print("Got new image at time:"+str(ros_image.header.stamp.secs))
+    
+    global odom_pub,motion,pixdiff,bridge,k,i_pose,threshold,rotation,translation,image_ref,image_next,new_cloud,kp2
+    
+    if motion==0:
+        
+        kp1,kp2,diff=initialize(image_ref,ros_image)
+        
+        if diff>=pixdiff:
+
+            motion=1
+            print('Motion Detected')
+            E,mask=cv2.findEssentialMat(kp2,kp1,k,cv2.RANSAC,prob=0.999,threshold=0.1, mask=None)
+            kp1=kp1[mask.ravel()==1]
+            kp2=kp2[mask.ravel()==1]    
+            print(kp1.shape[0],kp2.shape[0])
+
+            #Recover relative translation and rotation
+            retval,rmat,trans,mask=cv2.recoverPose(E,kp1,kp2,k)
+    
+             #Calculate the cloud of the next set
+            new_cloud=triangulation(rmat,trans,kp1,kp2,k)
+
+            print(new_cloud.shape)
+            
+            #Propagate translation and rotation
+            translation=translation+rotation.dot(trans)
+            rotation=rotation.dot(rmat)
+            
+            publisher(ros_image.header.stamp.secs, translation, rotation)
+
+
+        elif diff<pixdiff:
+
+            print("No motion detected,publishing previous pose")
+            i_pose.header.stamp.secs=ros_image.header.stamp.secs
+            odom_pub.publish(i_pose)
+
+    elif motion==1:
+
+        
+        old_cloud=new_cloud
+        kp1=kp2
+        image_ref=image_next
+        image_next=bridge.imgmsg_to_cv2(ros_image, "bgr8")
+        time=ros_image.header.stamp.secs
+
+        kp1,kp2,diff=track_features(image_ref,image_next,kp1)
+        print(kp1.shape[0],kp2.shape[0])
+        
+        if kp1.shape[0]<threshold:
+            kp2=extract_features(image_next)
+
+
+        #Calculate Essential Matrix and propagate inliers and outliers
+        E,mask=cv2.findEssentialMat(kp2,kp1,k,cv2.RANSAC,prob=0.999,threshold=0.4, mask=None)
+        kp1=kp1[mask.ravel()==1]
+        kp2=kp2[mask.ravel()==1]        
+
+        #Recover relative translation and rotation
+        retval,rmat,trans,mask=cv2.recoverPose(E,kp1,kp2,k)
+    
+        #Calculate the cloud of the next set
+        new_cloud=triangulation(rmat,trans,kp1,kp2,k)
+
+
+        #Compare the two clouds to recover the scale factor
+        scale= -RelativeScale(old_cloud, new_cloud)
+        #Propagate translation and rotation
+        translation=translation+scale*rotation.dot(trans)
+        rotation=rotation.dot(rmat)
+
+        publisher(time,translation,rotation)
+
+def calibration(camera_info):
+    
+    global k
+    
     k=camera_info.K
     k=np.array(k).reshape(3,3)
     print("Got Calibration matrix:")
     print(k)
 
-    #Initial Position of UAV
-    initial_pose=rospy.wait_for_message("/mavros/global_position/local",Odometry)
+def initial_info(initial_pose):
+
+    global i_pose,translation,rotation
+    i_pose=initial_pose
+    
     pos=[initial_pose.pose.pose.position.x,initial_pose.pose.pose.position.y,initial_pose.pose.pose.position.z]
     x=initial_pose.pose.pose.orientation.x
     y=initial_pose.pose.pose.orientation.y
@@ -35,151 +166,38 @@ def initialize():
     w=initial_pose.pose.pose.orientation.w
     euler=list(euler_from_quaternion([x,y,z,w]))
     print("Got initial position")
-
-    #Publishing initial pose
-
     odom_pub.publish(initial_pose)
     print("Initial Pose published")
 
-
     rotation=euler_matrix(euler[0],euler[1],euler[2],'sxyz')[:3,:3]         #Initial orientation in matrix form
-    translation=np.array(pos).reshape(3,1)             #Initial pose in array form   
+    translation=np.array(pos).reshape(3,1)                                  #Initial pose in array form   
     print("Intial position")
     print(translation)
     print("Intial orientation:")
     print(rotation)
 
-
-    #Get first image
-    image_initial=rospy.wait_for_message("/webcam/image_raw",Image)
-    image_initial = bridge.imgmsg_to_cv2(image_initial, "bgr8")
-    print("Got first image")
-    kp1=extract_features(image_initial)
-    diff=0
-    rospy.sleep(10)
-
-    #Get Next Image
-    while(diff<pixdiff):
-        image_next=rospy.wait_for_message("/webcam/image_raw",Image)
-        print("Got new image, checking for motion")
-        t=image_next.header.stamp
-        image_next=bridge.imgmsg_to_cv2(image_next, "bgr8")
-        kp2=extract_features(image_next)
-
-        #Check pixel difference to verify motion
-        kp1,kp2,diff=track_features(image_initial, image_next, kp1)
-        initial_pose.header.stamp=t
-        odom_pub.publish(initial_pose)
-    
-    print(kp1.shape[0],kp2.shape[0])
-    print("Motion detected...") 
-    E,mask=cv2.findEssentialMat(kp2,kp1,k,cv2.RANSAC,prob=0.999,threshold=0.1, mask=None)
-    kp1=kp1[mask.ravel()==1]
-    kp2=kp2[mask.ravel()==1]    
-
-    #Recover relative translation and rotation
-    retval,rmat,trans,mask=cv2.recoverPose(E,kp1,kp2,k)
-    
-    #Calculate the cloud of the next set
-    new_cloud=triangulation(rmat,trans,kp1,kp2,k)
-    
-
-    #Propagate translation and rotation
-    translation=translation+rotation.dot(trans)
-    rotation=rotation.dot(rmat)
-    
-    #Publish odometry values
-    odom=Odometry()
-    odom.header.stamp=t
-    rotation1=np.hstack((rotation,np.zeros((3,1))))
-    rotation1=np.vstack((rotation1,np.array([0,0,0,1]).reshape(1,4)))
-    euler=list(euler_from_matrix(rotation1,'sxyz'))
-    quat=quaternion_from_euler(euler[0],euler[1],euler[2])
-    position=list(translation.reshape(3,))
-    odom.pose.pose.position.x=position[0]
-    odom.pose.pose.position.y=position[1]
-    odom.pose.pose.position.z=position[2]
-    odom.pose.pose.orientation.x=quat[0]
-    odom.pose.pose.orientation.y=quat[1]
-    odom.pose.pose.orientation.z=quat[2]
-    odom.pose.pose.orientation.w=quat[3]
-    odom.pose.covariance=list(p_cov.reshape(36,))
-    odom_pub.publish(odom)
-
-    print("Time:",t)
-    print("Translation:",translation)
-    print("Rotation:",rotation)
-
-
-    while not rospy.is_shutdown():
-        #Define pose of Odometry type
-        bridge=CvBridge()
-        threshold=1000
-        pixdiff=2
-        
-        odom=Odometry()
-
-        old_cloud=new_cloud
-        kp1=kp2
-        image_ref=image_next
-
-        image_next=rospy.wait_for_message("/webcam/image_raw",Image)
-        print("Got new image")
-        odom.header.stamp=image_next.header.stamp #Time at which image was taken
-        print("Time:",odom.header.stamp)
-        image_next=bridge.imgmsg_to_cv2(image_next, "bgr8")
-
-        kp1,kp2,diff=track_features(image_ref,image_next,kp1)
-        print(kp1.shape[0],kp2.shape[0])
-        if diff>pixdiff:
-            if kp1.shape[0]<threshold:
-                kp2=extract_features(image_next)
-
-
-    #Calculate Essential Matrix and propagate inliers and outliers
-    E,mask=cv2.findEssentialMat(kp2,kp1,k,cv2.RANSAC,prob=0.999,threshold=0.1, mask=None)
-    kp1=kp1[mask.ravel()==1]
-    kp2=kp2[mask.ravel()==1]        
-
-    #Recover relative translation and rotation
-    retval,rmat,trans,mask=cv2.recoverPose(E,kp1,kp2,k)
-    
-    #Calculate the cloud of the next set
-    new_cloud=triangulation(rmat,trans,kp1,kp2,k)
-
-
-    #Compare the two clouds to recover the scale factor
-    scale= -RelativeScale(old_cloud, new_cloud)
-    #Propagate translation and rotation
-    translation=translation+scale*rotation.dot(trans)
-    rotation=rotation.dot(rmat)
-    
-    rotation1=np.hstack((rotation,np.zeros((3,1))))
-    rotation1=np.vstack((rotation1,np.array([0,0,0,1]).reshape(1,4)))
-    euler=list(euler_from_matrix(rotation1,'sxyz'))
-    quat=quaternion_from_euler(euler[0],euler[1],euler[2])
-    position=list(translation.reshape(3,))
-    odom.pose.pose.position.x=position[0]
-    odom.pose.pose.position.y=position[1]
-    odom.pose.pose.position.z=position[2]
-    odom.pose.pose.orientation.x=quat[0]
-    odom.pose.pose.orientation.y=quat[1]
-    odom.pose.pose.orientation.z=quat[2]
-    odom.pose.pose.orientation.w=quat[3]
-    odom.pose.covariance=list(p_cov.reshape(36,))
-    odom_pub.publish(odom)
-
-    print("Translation:",translation)
-    print("Rotation:",rotation)
-
-    if kp1.shape[0]<threshold:
-        kp2=extract_features(image_next)
-
-    
 def main(args):
+
+    global odom_pub,image_ref,bridge
+
     rospy.init_node('visual_odometry', anonymous=True)
-   
-    initialize()
-    rospy.spin()
+    camera_i=rospy.wait_for_message("/webcam/camera_info",CameraInfo)
+    calibration(camera_i)
+    
+    #Initial Position of UAV
+    initial_pos=rospy.wait_for_message("/mavros/global_position/local",Odometry)
+    initial_info(initial_pos)
+
+    image_ref=rospy.wait_for_message("/webcam/image_raw",Image)
+    image_ref=bridge.imgmsg_to_cv2(image_ref, "bgr8")
+    print("Got First Image")
+
+    try:
+        image_next=rospy.Subscriber("/webcam/image_raw",Image,image_callback,queue_size=100)
+        rospy.spin()
+        rospy.sleep(10)
+    except rospy.ROSInterruptException:
+        pass
+
 if __name__ == '__main__':
     main(sys.argv)
